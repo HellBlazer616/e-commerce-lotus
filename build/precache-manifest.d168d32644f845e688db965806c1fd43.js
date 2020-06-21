10c2bef3a8fcd958fb3ff129552a667d5590f496
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2020-06-21 20:27:40 +0600

    added build version

---
+++ build/precache-manifest.d168d32644f845e688db965806c1fd43.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da97a027320c452ab84298cf7632460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aba3b8808ca4edb37687",
+    "url": "/static/css/main.8d6cae66.chunk.css"
+  },
+  {
+    "revision": "a411694ea8f137674076",
+    "url": "/static/js/2.a2f8f97f.chunk.js"
+  },
+  {
+    "revision": "b57315b8a35d9900d44ee0427482ac79",
+    "url": "/static/js/2.a2f8f97f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba3b8808ca4edb37687",
+    "url": "/static/js/main.7bc0acae.chunk.js"
+  },
+  {
+    "revision": "39d502c0600acb131b17",
+    "url": "/static/js/runtime-main.a3934485.js"
+  },
+  {
+    "revision": "686db4a57c4bd5613db72ab863d72bb5",
+    "url": "/static/media/background.686db4a5.jpg"
+  },
+  {
+    "revision": "928ac13e18421baaf316ed266481c628",
+    "url": "/static/media/enter.928ac13e.svg"
+  },
+  {
+    "revision": "199c3849b4ab00ffaca0b3c7fecdfbea",
+    "url": "/static/media/registerSvg.199c3849.svg"
+  },
+  {
+    "revision": "31f87a86769045a27539af5e1bf85e23",
+    "url": "/static/media/reset.31f87a86.svg"
+  },
+  {
+    "revision": "d039497d619fbc099f432d174a2b008d",
+    "url": "/static/media/update.d039497d.svg"
+  },
+  {
+    "revision": "b97f4ccb31c73f56d0f3708e116558e9",
+    "url": "/static/media/vegetable.b97f4ccb.svg"
+  }
+]);
